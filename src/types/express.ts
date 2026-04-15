46dea5a38607c4814d682fae04159b469bd3380a
Benja Aguilar <benjaaguilar02@gmail.com>
2026-04-15 18:19:23 -0300

    types: add express types

---
+++ src/types/express.ts
@@ -0,0 +1,7 @@
+import express from "express";
+
+export type Request = express.Request;
+export type Response = express.Response;
+export type Next = express.NextFunction;
+export type RequestHandler = express.RequestHandler;
+export type Error = express.ErrorRequestHandler;
