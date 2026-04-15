3079a4531e86296340aaeb702e3b1e581739e400
Benja Aguilar <benjaaguilar02@gmail.com>
2026-04-15 19:39:22 -0300

    chore: rename files

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import userRouter from "./userRoutes";
+import userRouter from "./user.routes";
 const router = Router();
 
 router.use("/user", userRouter);
--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/userController";
+import { registerUser } from "../controllers/user.controller";
 const userRouter = Router();
 
 userRouter.post("/create", registerUser);
