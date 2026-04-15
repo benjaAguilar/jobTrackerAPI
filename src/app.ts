4214e3cf241b1d1b337007320bef2571dfa2a19c
Benja Aguilar <benjaaguilar02@gmail.com>
2026-04-15 19:34:43 -0300

    feat: add createServices() to app.locals.services
    now we have access to all the services of our app on the controllers
    without knowing the actual service that depends on. making it reusable,
    scalable and testeable

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import router from "./routes";
 import { errorHandler } from "./controllers/errorHandler";
+import { createServices } from "./services";
 
 export const app = express();
 
@@ -9,6 +10,9 @@ dotenv.config();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+const services = createServices();
+app.locals.services = services;
 
 app.use(router);
 
