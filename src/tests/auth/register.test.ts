d94730ea87f22b28e549b527bbb13ceb06e0c9d3
Benja Aguilar <benjaaguilar02@gmail.com>
2026-04-16 19:32:06 -0300

    remove: test files on src

--- src/tests/auth/register.test.ts
+++
@@ -1,8 +0,0 @@
-import request from "supertest";
-import { app } from "../../app";
-
-describe("POST /api/user/register", () => {
-  describe("success", () => {
-    it("should create an user with valid data", async () => {});
-  });
-});
