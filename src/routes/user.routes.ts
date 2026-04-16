27e7d12cf6ce5ae157667ace8e368d82d9eca8f2
Benja Aguilar <benjaaguilar02@gmail.com>
2026-04-16 13:22:01 -0300

    chore: replace /create for /register

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -2,6 +2,6 @@ import { Router } from "express";
 import { registerUser } from "../controllers/user.controller";
 const userRouter = Router();
 
-userRouter.post("/create", registerUser);
+userRouter.post("/register", registerUser);
 
 export default userRouter;
