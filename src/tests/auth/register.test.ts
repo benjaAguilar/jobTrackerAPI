05da602766b5f468dc36069df066dd2c0b0b52fa
Benja Aguilar <benjaaguilar02@gmail.com>
2026-04-16 13:22:17 -0300

    test: add user register tests

---
+++ src/tests/auth/register.test.ts
@@ -0,0 +1,8 @@
+import request from "supertest";
+import { app } from "../../app";
+
+describe("POST /api/user/register", () => {
+  describe("success", () => {
+    it("should create an user with valid data", async () => {});
+  });
+});
