f6ee1862f1a7ba9267a84153bd13ce9c73389e06
Benja Aguilar <benjaaguilar02@gmail.com>
2026-04-16 13:21:44 -0300

    feat: add username and email checks
    this checks if some of the fields already exists on db throwing an error

--- src/repositories/prisma/prismaUser.repository.ts
+++ src/repositories/prisma/prismaUser.repository.ts
@@ -8,11 +8,24 @@ export class PrismaUser implements UserRepository {
     this.prisma = prisma;
   }
 
-  create(data: {
+  async create(data: {
     username: string;
     password: string;
     email: string;
   }): Promise<User> {
+    const [userByUsername, userByEmail] = await Promise.all([
+      this.prisma.user.findUnique({ where: { username: data.username } }),
+      this.prisma.user.findUnique({ where: { email: data.email } }),
+    ]);
+
+    if (userByUsername) {
+      throw new Error("Username is already taken");
+    }
+
+    if (userByEmail) {
+      throw new Error("Email is already taken");
+    }
+
     return this.prisma.user.create({ data });
   }
 }
