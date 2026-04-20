25277eb0f1d98463a361377c0fdbb5dacc95e22e
Benja Aguilar <benjaaguilar02@gmail.com>
2026-04-20 17:02:05 -0300

    feat: add bcrypt.hash feature
    this function hashes and return the hashed password

--- src/utils/bcrypt.ts
+++ src/utils/bcrypt.ts
@@ -1,5 +1,5 @@
 import bcrypt from "bcryptjs";
-import { CustomError } from "./customError";
+import { CustomError, CustomServerError } from "./customError";
 
 export async function comparePasswords(
   inputPassword: string,
@@ -8,4 +8,9 @@ export async function comparePasswords(
   const match = await bcrypt.compare(inputPassword, hashedPassword);
 
   if (!match) throw new CustomError("Incorrect Password", 400);
+}
+
+export async function hashPassword(password: string): Promise<string> {
+  const hash = await bcrypt.hash(password, 10);
+  return hash;
 }
