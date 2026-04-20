433532c156088552c8fce8d761222a364c63a206
Benja Aguilar <benjaaguilar02@gmail.com>
2026-04-20 13:06:41 -0300

    feat: add a customServerError

--- src/utils/customError.ts
+++ src/utils/customError.ts
@@ -27,3 +27,14 @@ export class CustomValidationError extends Error {
     Object.setPrototypeOf(this, CustomValidationError.prototype);
   }
 }
+
+export class CustomServerError extends Error {
+  public statusCode: number;
+
+  constructor(message: string, statusCode: number) {
+    super(message);
+    this.statusCode = statusCode;
+
+    Object.setPrototypeOf(this, CustomServerError.prototype);
+  }
+}
