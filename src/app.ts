39eb5340cfe06a0b783669de5aba5f8dc983e93e
Benja Aguilar <benjaaguilar02@gmail.com>
2026-04-20 14:50:36 -0300

    feat: add cookie parser and passport config

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,11 @@
 import express from "express";
 import dotenv from "dotenv";
 import router from "./routes";
+import cookieParser from "cookie-parser";
 import { errorHandler } from "./controllers/errorHandler";
 import { createServices } from "./services";
+import configurePassport from "./lib/passport";
+import passport from "passport";
 
 export const app = express();
 
@@ -10,9 +13,13 @@ dotenv.config();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 const services = createServices();
 app.locals.services = services;
+
+configurePassport(passport, services.userService);
+app.use(passport.initialize());
 
 app.use("/api", router);
 
