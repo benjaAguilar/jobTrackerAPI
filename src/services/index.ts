f7fb1c41b54341419fc716284188db9b9b646588
Benja Aguilar <benjaaguilar02@gmail.com>
2026-04-21 14:50:07 -0300

    feat: add jobService and techService to app services

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,10 +1,16 @@
 import { prisma } from "../lib/prisma";
+import { PrismaJob } from "../repositories/prisma/prismaJob.repository";
+import { PrismaTech } from "../repositories/prisma/prismaTech.repository";
 import { PrismaUser } from "../repositories/prisma/prismaUser.repository";
+import { JobService } from "./job.service";
+import { TechService } from "./tech.service";
 import { UserService } from "./user.service";
 
 export function createServices() {
   return {
     userService: new UserService(new PrismaUser(prisma)),
+    jobService: new JobService(new PrismaJob(prisma)),
+    techService: new TechService(new PrismaTech(prisma)),
   };
 }
 
