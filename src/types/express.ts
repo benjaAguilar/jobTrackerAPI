d3bfaf0cfcb2bb128f8e896b0fa04f3fa15606c7
Benja Aguilar <benjaaguilar02@gmail.com>
2026-04-21 14:50:40 -0300

    types: add AuthenticatedRequest type
    this includes the User on req

--- src/types/express.ts
+++ src/types/express.ts
@@ -1,7 +1,11 @@
 import express from "express";
+import { User } from "../../generated/prisma/client";
 
 export type Request = express.Request;
 export type Response = express.Response;
 export type Next = express.NextFunction;
 export type RequestHandler = express.RequestHandler;
 export type Error = express.ErrorRequestHandler;
+export interface AuthenticatedRequest extends Request {
+  user: User;
+}
