00ab06f8bf8d4944942fd932390959808d4a12c7
Benja Aguilar <benjaaguilar02@gmail.com>
2026-04-21 14:48:59 -0300

    feat: add PrismaJob implementation
    add create function wich creates a new job via prisma

---
+++ src/repositories/prisma/prismaJob.repository.ts
@@ -0,0 +1,34 @@
+import { Job, JobState, PrismaClient } from "../../../generated/prisma/client";
+import { JobRepository } from "../job.repository";
+
+export class PrismaJob implements JobRepository {
+  private prisma: PrismaClient;
+
+  constructor(prisma: PrismaClient) {
+    this.prisma = prisma;
+  }
+
+  async create(data: {
+    vacantName: string;
+    company: string;
+    notes?: string;
+    techs: number[];
+    state: JobState;
+    userId: number;
+    rejectionReason?: string;
+  }): Promise<Job> {
+    return this.prisma.job.create({
+      data: {
+        vacantName: data.vacantName,
+        company: data.company,
+        notes: data.notes,
+        state: data.state,
+        user_id: data.userId,
+        rejectionReason: data.rejectionReason,
+        techsRequired: {
+          create: data.techs.map((techId) => ({ tech_id: techId })),
+        },
+      },
+    });
+  }
+}
