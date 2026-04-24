65e3b7ad6fdcdb775eb5a67df4d70eca1cd8bd07
Benja Aguilar <benjaaguilar02@gmail.com>
2026-04-24 17:54:31 -0300

    feat: add job update routes

--- src/routes/job.routes.ts
+++ src/routes/job.routes.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { jwtAuthStrategy } from "../middlewares/passport";
-import { create, getJobs } from "../controllers/job.controller";
+import { create, getJobs, update } from "../controllers/job.controller";
 const jobRouter = Router();
 
 jobRouter.post("/create", jwtAuthStrategy, create);
 jobRouter.get("/", jwtAuthStrategy, getJobs);
+jobRouter.put("/:jobId", jwtAuthStrategy, update);
 
 export default jobRouter;
