fcbf055aee6f4a35e9a4bbc124b0da861ba8f2e8
Benja Aguilar <benjaaguilar02@gmail.com>
2026-04-24 16:31:09 -0300

    ref: refactor createOrGetMultipleTechs
    ref to pass all the tests

--- src/services/tech.service.ts
+++ src/services/tech.service.ts
@@ -1,6 +1,8 @@
 import { Tech } from "../../generated/prisma/client";
 import { TechRepository } from "../repositories/tech.repository";
+import { CustomError } from "../utils/customError";
 import { normalizeTechs } from "../utils/normalizeTech";
+import { parseJson } from "../utils/utils";
 
 export class TechService implements TechRepository {
   private techRepo: TechRepository;
@@ -10,13 +12,27 @@ export class TechService implements TechRepository {
   }
 
   async createOrGetMultipleTechs(techs: string): Promise<number[]> {
-    return await Promise.all(
-      JSON.parse(techs).map(async (tech: string) => {
+    const parsedTechs = parseJson(techs);
+
+    if (!parsedTechs || !Array.isArray(parsedTechs)) {
+      throw new CustomError("Please provide techs in an allowed format", 400);
+    }
+
+    const [hasOtherType] = parsedTechs.filter(
+      (val: any) => typeof val !== "string",
+    );
+    if (hasOtherType) {
+      throw new CustomError("Techs should be on string format", 400);
+    }
+
+    const arrOfTechs = await Promise.all(
+      parsedTechs.map(async (tech: string) => {
         const normalizedTech = normalizeTechs(tech);
         const tec = await this.techRepo.createOrGet(normalizedTech);
         return tec.id;
       }),
     );
+    return [...new Set(arrOfTechs)];
   }
 
   async createOrGet(techName: string): Promise<Tech> {
