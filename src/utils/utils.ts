3688d35d94d983cf013280528bdc0f8533396c9a
Benja Aguilar <benjaaguilar02@gmail.com>
2026-04-24 17:52:27 -0300

    feat: add parseParamId Util
    this util parses the param provided on a url.
    if param is invalid throws an error
    if is valid returns a number id

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,3 +1,5 @@
+import { CustomError } from "./customError";
+
 export function parseJson(str: string): object | null {
   try {
     const json = JSON.parse(str);
@@ -5,4 +7,11 @@ export function parseJson(str: string): object | null {
   } catch (e) {
     return null;
   }
+}
+
+export function parseParamId(param: string | string[]): number {
+  if (Array.isArray(param) || isNaN(parseInt(param)))
+    throw new CustomError("Provide a valid parameter id", 400);
+
+  return parseInt(param);
 }
