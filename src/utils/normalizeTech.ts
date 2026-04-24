1f1c150cb171e914b1610b35a5dd7d2ad30d2642
Benja Aguilar <benjaaguilar02@gmail.com>
2026-04-24 10:55:27 -0300

    ref: refactor normalizeTechs to pass tests

--- src/utils/normalizeTech.ts
+++ src/utils/normalizeTech.ts
@@ -1,5 +1,11 @@
 export function normalizeTechs(techName: string): string {
   const arrOfWords = techName.trim().toLocaleLowerCase().split("");
-  arrOfWords[0].toLocaleUpperCase();
+  const hasEnter = arrOfWords.findIndex((val) => val === "\n");
+
+  if (hasEnter > 0) {
+    arrOfWords.splice(hasEnter, 1);
+  }
+
+  arrOfWords[0] = arrOfWords[0].toLocaleUpperCase();
   return arrOfWords.join("");
 }
