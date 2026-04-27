d50cc5340b2a677ecdff5124d4e33e63af1e0fa9
Benja Aguilar <benjaaguilar02@gmail.com>
2026-04-27 10:43:40 -0300

    feat: add deleteJob controller route

--- src/routes/job.routes.ts
+++ src/routes/job.routes.ts
@@ -1,10 +1,16 @@
 import { Router } from "express";
 import { jwtAuthStrategy } from "../middlewares/passport";
-import { create, getJobs, update } from "../controllers/job.controller";
+import {
+  create,
+  deleteJob,
+  getJobs,
+  update,
+} from "../controllers/job.controller";
 const jobRouter = Router();
 
 jobRouter.post("/create", jwtAuthStrategy, create);
 jobRouter.get("/", jwtAuthStrategy, getJobs);
 jobRouter.put("/:jobId", jwtAuthStrategy, update);
+jobRouter.delete("/:jobId", jwtAuthStrategy, deleteJob);
 
 export default jobRouter;
