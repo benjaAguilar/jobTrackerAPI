65154d65a68ea2eee5b720d469854610c8d93d48
Benja Aguilar <benjaaguilar02@gmail.com>
2026-04-27 12:54:35 -0300

    feat: add logout route

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
-import { login, register } from "../controllers/auth.controller";
+import { login, logout, register } from "../controllers/auth.controller";
+import { jwtAuthStrategy } from "../middlewares/passport";
 const authRouter = Router();
 
 authRouter.post("/login", login);
 authRouter.post("/register", register);
+authRouter.post("/logout", jwtAuthStrategy, logout);
 
 export default authRouter;
