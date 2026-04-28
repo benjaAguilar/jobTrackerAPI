d310780eef7818330657e27e5d24e48fe28c1e76
Benja Aguilar <benjaaguilar02@gmail.com>
2026-04-28 17:02:27 -0300

    feat: add GET /job/event route

--- src/routes/job.routes.ts
+++ src/routes/job.routes.ts
@@ -3,6 +3,7 @@ import { jwtAuthStrategy } from "../middlewares/passport";
 import {
   create,
   deleteJob,
+  getJobEvents,
   getJobs,
   update,
 } from "../controllers/job.controller";
@@ -10,6 +11,7 @@ const jobRouter = Router();
 
 jobRouter.post("/create", jwtAuthStrategy, create);
 jobRouter.get("/", jwtAuthStrategy, getJobs);
+jobRouter.get("/event", jwtAuthStrategy, getJobEvents);
 jobRouter.put("/:jobId", jwtAuthStrategy, update);
 jobRouter.delete("/:jobId", jwtAuthStrategy, deleteJob);
 
